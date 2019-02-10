2595e069fdc785bf11ee59b92d6c31b15cabd9ce
Zhang Tian <tian.zhang@rea-group.com>
2019-02-10 23:03:36 +0800

    Refact test

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,13 +2,12 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 0) {
-            return 1;
-        } else if (i == 3) {
-            return 6;
-        } else if (i < 0) {
+        if (i < 0) {
             throw new IllegalArgumentException();
+        } else if (i == 1 || i == 0) {
+            return 1;
+        } else {
+            return i * (i - 1);
         }
-        return i;
     }
 }
