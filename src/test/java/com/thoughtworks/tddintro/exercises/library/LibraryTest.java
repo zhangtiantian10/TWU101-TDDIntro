90c01ce561c8d81064141c75b4337784daa529e6
Zhang Tian <tian.zhang@rea-group.com>
2019-02-12 13:16:12 +0800

    Compile the library test.

--- src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
@@ -32,27 +32,30 @@ public class LibraryTest {
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldPrintBookTitleWhenThereIsOneBook() {
         String title = "Book Title";
         books.add(title);
 
         library.listBooks();
 
-        // add a verify statement here that shows that the book title was printed by the printStream
+        verify(printStream).println(title + "\n");
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldPrintNothingWhenThereAreNoBooks() {
+        library.listBooks();
 
-        // implement me
+        verify(printStream).println("");
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldPrintBothBookTitlesWhenThereAreTwoBooks() throws IOException {
-        // implement me
+        books.add("One");
+        books.add("Two");
+
+        library.listBooks();
+
+        verify(printStream).println("One\nTwo\n");
     }
 
     @Test
@@ -63,9 +66,8 @@ public class LibraryTest {
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldDeleteBookFromCollectionWhenRemovedByUser() throws IOException {
-        // Add when/thenReturn here
+        when(bufferedReader.readLine()).thenReturn("The Two Towers");
 
         books.add("The Two Towers");
         library.removeBook();
