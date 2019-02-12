1a836aa2731291dcd10d10d9df750e336343081b
Zhang Tian <tian.zhang@rea-group.com>
2019-02-12 10:11:00 +0800

    Compile the account deposit money.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,24 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int money;
+
+    public Account() {}
+
+    public Account(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int deposit(int money) {
+        this.money = this.money + money;
+        return this.money;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,11 +3,14 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        Account account = new Account(50);
+        assertThat(account.deposit(100), is(150));
     }
 
     @Test
